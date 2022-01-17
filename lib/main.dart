47b935dcce5e83aa30dd1154587579a70d2c0696
Everton Pires <everton.pires@in8.com.br>
2022-01-17 10:57:23 -0300

    add run oznded guarded

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -5,15 +7,20 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'app/src/app_module.dart';
 import 'app/src/app_widget.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(
-      ModularApp(
-        module: AppModule(),
-        child: const AppWidget(),
-      ),
-    );
-  });
+// TODO: call the firebase when its possible
+Future<void> main() async {
+  runZonedGuarded(() {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterError.onError = (FlutterErrorDetails errorDetails) {};
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+        .then((_) {
+      runApp(
+        ModularApp(
+          module: AppModule(),
+          child: const AppWidget(),
+        ),
+      );
+    });
+  }, (Object error, StackTrace stackTrace) {});
 }
